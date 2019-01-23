20e6d6265a5bdd30d31ef332dfc68510d09d29ee
harryherold <harryherold@googlemail.com>
2019-01-23 13:01:34 +0100

    Split access sampling in src and header.

---
+++ include/access_sampling.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <scorep/plugin/plugin.hpp>
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <vector>
+
+#include <perf_sampling.h>
+
+extern std::vector<BufferPtr> thread_buffers;
+
+using namespace scorep::plugin::policy;
+using TimeValuePair = std::pair<scorep::chrono::ticks, double>;
+using MetricProperty = scorep::plugin::metric_property;
+
+class access_sampling : public scorep::plugin::base<access_sampling, async, per_thread, scorep_clock>
+{
+    public:
+    access_sampling ();
+
+    std::vector<MetricProperty> get_metric_properties (const std::string &metric_name);
+
+    int32_t add_metric (const std::string &event);
+
+    void start ()
+    {}
+
+    void stop ()
+    {}
+
+    template <typename CursorType> void get_all_values (int32_t id, CursorType &cursor);
+
+    private:
+    PerfSampling perf_sampling_;
+    PfmWrapper pfm_wrapper_;
+};
+
+template <typename CursorType> void access_sampling::get_all_values (int32_t id, CursorType &cursor)
+{
+    std::cout << "Trace Buffer of thread: " << thread_buffers[id]->tid << '\n';
+}
--- src/access_sampling.cpp
+++ src/access_sampling.cpp
@@ -5,71 +5,44 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <tuple>
 #include <vector>
 
+#include <access_sampling.h>
 #include <perf_sampling.h>
 
-
-using namespace scorep::plugin::policy;
 
 std::mutex buffer_mutex;
 std::vector<BufferPtr> thread_buffers;
 
-class access_sampling : public scorep::plugin::base<access_sampling, async, per_thread, scorep_clock>
+
+access_sampling::access_sampling () : perf_sampling_ (), pfm_wrapper_ ()
 {
-    public:
-    using TimeValuePair = std::pair<scorep::chrono::ticks, double>;
-    using MetricProperty = scorep::plugin::metric_property;
+    std::cout << "Loading Metric Plugin\n";
+}
 
-    access_sampling () : perf_sampling_ (), pfm_wrapper_ ()
+std::vector<MetricProperty> access_sampling::get_metric_properties (const std::string &metric_name)
+{
+    std::cout << "get metric properties called with: " << metric_name << '\n';
+
+    std::vector<MetricProperty> metric_properties;
+
+    if (pfm_wrapper_.metric_is_supported (metric_name))
     {
-        std::cout << "Loading Metric Plugin\n";
+        metric_properties.push_back (MetricProperty (metric_name, "", "Address").absolute_point ().value_uint ());
     }
 
-    std::vector<MetricProperty> get_metric_properties (const std::string &metric_name)
-    {
-        std::cout << "get metric properties called with: " << metric_name << '\n';
+    return metric_properties;
+}
 
-        std::vector<MetricProperty> metric_properties;
-
-        if (pfm_wrapper_.metric_is_supported(metric_name))
-        {
-            metric_properties.push_back (
-            MetricProperty (metric_name, "", "Address").absolute_point ().value_uint ());
-        }
-
-        return metric_properties;
-    }
-
-    int32_t add_metric (const std::string &event)
-    {
-        std::cout << "add metric called with: " << event << '\n';
-        BufferPtr buffer = std::make_shared<TraceBuffer> ();
-
-        std::lock_guard<std::mutex> lock (buffer_mutex);
-        thread_buffers.push_back (buffer);
-        int32_t id = thread_buffers.size () - 1;
-        return id;
-    }
-
-    void start ()
-    {
-        std::cout << "Start\n";
-    }
-
-    void stop ()
-    {
-        std::cout << "Stop\n";
-    }
-
-    template <typename C> void get_all_values (int32_t id, C &cursor)
-    {
-        std::cout << "Trace Buffer of thread: " << thread_buffers[id]->tid << '\n';
-    }
-
-    private:
-    PerfSampling perf_sampling_;
-    PfmWrapper pfm_wrapper_;
-};
+int32_t access_sampling::add_metric (const std::string &event)
+{
+    std::cout << "add metric called with: " << event << '\n';
+    BufferPtr buffer = std::make_shared<TraceBuffer> ();
+    std::lock_guard<std::mutex> lock (buffer_mutex);
+    thread_buffers.push_back (buffer);
+    int32_t id = thread_buffers.size () - 1;
+    return id;
+}
 
 SCOREP_METRIC_PLUGIN_CLASS (access_sampling, "access_sampling")
