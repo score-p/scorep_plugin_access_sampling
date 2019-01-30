cb99a9a351794a17c256771f6fbdbfe294e73fb2
harryherold <harryherold@googlemail.com>
2019-01-30 13:27:27 +0100

    Move ring buffer allocation in class.

--- include/trace_buffer.h
+++ include/trace_buffer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <boost/circular_buffer.hpp>
 #include <thread>
 
 extern "C"
@@ -7,18 +8,27 @@ extern "C"
 #include <sys/mman.h>
 #include <unistd.h>
 }
-
 namespace perf_buffer
 {
+
 constexpr std::size_t PERF_SAMPLE_MAX_SIZE = 64;
 
 struct PerfRingBuffer
 {
+    explicit PerfRingBuffer (int fd);
     int moved;
     void *base;
     int mask;
     uint64_t prev;
     char event_copy[PERF_SAMPLE_MAX_SIZE] __attribute__ ((aligned (8)));
+
+    /**
+     * Length of the memory mapping
+     * mmap size should be 1+2^n pages,
+     * first page is a metadata page
+     */
+    const std::size_t PAGE_SIZE = sysconf (_SC_PAGESIZE);
+    static constexpr std::size_t BUFFERSIZE_DEFAULT = 64;
 };
 
 
@@ -28,7 +38,5 @@ struct TraceBuffer
     int fd;
     PerfRingBuffer ring_buffer;
 };
-
-void allocate_ring_buffer (TraceBuffer * trace_buffer);
 
 } // namespace perf_buffer
--- src/trace_buffer.cpp
+++ src/trace_buffer.cpp
@@ -1,24 +1,18 @@
 #include <trace_buffer.h>
 
-namespace perf_buffer{
-
-/**
- * Length of the memory mapping
- * mmap size should be 1+2^n pages,
- * first page is a metadata page
- */
-static const std::size_t PAGE_SIZE = sysconf (_SC_PAGESIZE);
-constexpr std::size_t BUFFERSIZE_DEFAULT = 64;
-void allocate_ring_buffer (TraceBuffer * trace_buffer)
+namespace perf_buffer
 {
-    trace_buffer->ring_buffer.moved = 0;
-    trace_buffer->ring_buffer.mask = BUFFERSIZE_DEFAULT * PAGE_SIZE - 1;
 
-    trace_buffer->ring_buffer.base = mmap (NULL, (BUFFERSIZE_DEFAULT + 1) * PAGE_SIZE,
-                                    PROT_READ | PROT_WRITE, MAP_SHARED, trace_buffer->fd, 0);
-    if (trace_buffer->ring_buffer.base == MAP_FAILED)
+PerfRingBuffer::PerfRingBuffer (int fd)
+{
+    moved = 0;
+    mask = BUFFERSIZE_DEFAULT * PAGE_SIZE - 1;
+
+    base = mmap (NULL, (BUFFERSIZE_DEFAULT + 1) * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if (base == MAP_FAILED)
     {
-        throw std::runtime_error("Error: Failed to allocate ring buffer.");
+        throw std::runtime_error ("Error: Failed to allocate ring buffer.");
     }
 }
 
